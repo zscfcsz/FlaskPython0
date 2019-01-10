7ac5102694fed65c901d691c97e5b6512011f8b4
zscfcsz <0testjp@gmail.com>
2019-01-10 14:56:21 +0800

    2019/01/10 1. Hello World Test.

---
+++ app.py
@@ -0,0 +1,20 @@
+import os;
+
+
+from flask import Flask;
+app = Flask(__name__);
+
+
+@app.route('/')
+def hello():
+        return 'Hello World!';
+
+
+if __name__ == '__main__':
+        # Bind to PORT if defined, otherwise default to 5000.
+        port = int(os.environ.get('PORT', 5000));
+        app.run(host='0.0.0.0', port=port);
+
+# http://blog.changyy.org/2012/02/python-heroku-windows-7.html
+# 待看 heroku 的部份?
+
