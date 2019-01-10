48a385619f28fae81d39ffc48baaf436b8f1d989
zscfcsz <0testjp@gmail.com>
2019-01-10 17:59:38 +0800

    2019/01/10 1. add line bot.

--- app.py
+++ app.py
@@ -1,20 +1,49 @@
-import os;
+from flask import Flask, request, abort
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
-from flask import Flask;
-app = Flask(__name__);
+app = Flask(__name__)
 
+# Channel Access Token
+line_bot_api = LineBotApi(os.environ['channel_access_token'])
+# Channel Secret
+handler = WebhookHandler(os.environ['channel_secret'])
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
 @app.route('/')
 def hello():
         return 'Hello World!';
 
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
-if __name__ == '__main__':
-        # Bind to PORT if defined, otherwise default to 5000.
-        port = int(os.environ.get('PORT', 5000));
-        app.run(host='0.0.0.0', port=port);
-
-# http://blog.changyy.org/2012/02/python-heroku-windows-7.html
-# 待看 heroku 的部份?
-
+# pip freeze > requirements.txt
+# echo web: python app.py > Procfile
